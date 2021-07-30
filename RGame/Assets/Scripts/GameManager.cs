d558de4edee789bed565a7032aa6f8f617105aec
unknown <uudquark@soongsil.ac.kr>
2021-07-30 20:37:32 +0900

    y
    g

---
+++ RGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+
+    public AudioSource theMusic;
+
+    public bool startPlaying;
+    public BeatScroller theBS;
+
+    public static GameManager instance;
+
+
+    public int currentScore;
+    
+    public static int AttackDamage = 200;
+
+    public int scorePerNote = AttackDamage / 2;
+    public int scorePerGoodNote = AttackDamage;
+    public int scorePerPerfectNote = AttackDamage * 2;
+
+
+
+    public int currentCombo;
+    public int comboTracker;
+    public int[] comboThresholds;
+
+    public Text scoreText; 
+    public Text comboText;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+        scoreText.text = "Score: 0";
+        currentCombo = 1;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(!startPlaying)
+        {
+            if(Input.anyKeyDown)
+            {
+                startPlaying = true;
+                theBS.hasStarted = true;
+
+                theMusic.Play();
+            }
+        }
+    }
+
+    public void NoteHit()
+    {
+        Debug.Log("Hit On Time");
+
+        if (currentCombo - 1 < comboThresholds.Length)
+        {
+            comboTracker++;
+
+            if (comboThresholds[currentCombo - 1] <= comboTracker)
+            {
+                comboTracker = 0;
+                currentCombo++; 
+            }
+        }
+
+        comboText.text = "Combo: x" + currentCombo;
+
+        //currentScore += scorePerNote * currentCombo;
+        scoreText.text = "Score: " + currentScore;
+    }
+
+    public void NormalHit()
+    {
+        currentScore += scorePerNote * currentCombo;
+        NoteHit();
+    }
+
+    public void GoodHit()
+    {
+        currentScore += scorePerGoodNote * currentCombo;
+        NoteHit();
+    }
+
+    public void PerfectHit()
+    {
+        currentScore += scorePerPerfectNote * currentCombo;
+        NoteHit();
+    }
+
+    public void NoteMissed()
+    {
+        Debug.Log("Missed Note");
+
+        currentCombo = 1;
+        comboTracker = 0;
+
+        comboText.text = "Combo: x" + currentCombo;
+    }
+
+
+}
---
+++ RGame/Assets/Scripts/NoteObject.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NoteObject : MonoBehaviour
+{
+    public bool canBePressed;
+
+    public KeyCode keyToPress;
+
+    public GameObject hitEffect, goodEffect, perfectEffect, missEffect;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+          if(Input.GetKeyDown(keyToPress))
+          {
+            if(canBePressed)
+            {
+                gameObject.SetActive(false);
+
+                //GameManager.instance.NoteHit(); 
+                
+                if(Mathf.Abs(transform.position.y)>0.25) //버튼과 노트 사이의 거리가 -0.25보다 크고 0.25보다 작을때 Hit
+                { 
+                    Debug.Log("Hit");
+                    GameManager.instance.NormalHit();
+                    Instantiate(hitEffect, transform.position, hitEffect.transform.rotation);
+                }
+                else if(Mathf.Abs(transform.position.y)>0.05f) //버튼과 노트 사이의 거리가 -0.05보다 크고 0.05보다 작을 때 Hit
+                {
+                    Debug.Log("Good");
+                    GameManager.instance.GoodHit();
+                    Instantiate(goodEffect, transform.position, goodEffect.transform.rotation);
+                }
+                else //버튼과 노트 사이의 거리가 -0.25~0.25까지 일 때 Hit
+                {
+                    Debug.Log("Perfect");
+                    GameManager.instance.PerfectHit();
+                    Instantiate(perfectEffect, transform.position, perfectEffect.transform.rotation);
+                }
+            }
+          }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.tag == "Activator")
+        {
+            canBePressed = true;
+
+        }
+    }
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if(gameObject.activeSelf)
+        {
+            if (other.tag == "Activator")
+            {
+                canBePressed = false;
+
+                GameManager.instance.NoteMissed();
+                Instantiate(missEffect, transform.position, missEffect.transform.rotation);
+
+            }
+        }
+        
+    }
+}
